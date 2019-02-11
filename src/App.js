15e444e4b9d079488cb3669d0a51b085afda1f28
ajinbramantio <ajinbramantio@gmail.com>
2019-02-11 16:45:41 +0700

    new project react js

--- src/App.js
+++ src/App.js
@@ -1,28 +1,31 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
 
 class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      data: [
+        { text: 'running together' },
+        { text: 'go to Hospital' },
+        { text: 'going to impactbyte' }
+      ]
+    }
+  }
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h1>Learn React</h1>
+        <header className="App-header" />
+        <ul>
+          {this.state.data.map(item => {
+            console.log(item)
+
+            return <li>{item.text}</li>
+          })}
+        </ul>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
